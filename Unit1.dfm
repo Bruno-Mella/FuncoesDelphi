7ff79ba9ce595290052fa5b253cf4f18a3fe5fd8
Bruno-Mella <150836633+Bruno-Mella@users.noreply.github.com>
2024-09-02 15:38:59 -0300

    Função para gerar um QRcode de uma string.

---
+++ Unit1.dfm
@@ -0,0 +1,48 @@
+object Form1: TForm1
+  Left = 463
+  Top = 184
+  Width = 644
+  Height = 534
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Image1: TImage
+    Left = 24
+    Top = 16
+    Width = 601
+    Height = 409
+    Stretch = True
+  end
+  object Edit1: TEdit
+    Left = 24
+    Top = 432
+    Width = 601
+    Height = 21
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 456
+    Top = 464
+    Width = 75
+    Height = 25
+    Caption = 'criar'
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 536
+    Top = 464
+    Width = 75
+    Height = 25
+    Caption = 'sair'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,80 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls, ACBrDelphiZXingQRCode;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Edit1: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+function CriarQRcodeBitmap(Texto: String): TBitmap;
+var
+  QRCode: TDelphiZXingQRCode;
+  QRCodeBitmap: TBitmap;
+  Row, Column: Integer;
+begin
+  QRCode := TDelphiZXingQRCode.Create;
+  QRCodeBitmap := TBitmap.Create;
+  try
+    QRCode.Encoding := qrUTF8BOM;
+    QRCode.QuietZone := 4;  // Ajuste conforme necessário
+    QRCode.Data := WideString(Texto);
+
+    QRCodeBitmap.Width  := QRCode.Columns;
+    QRCodeBitmap.Height := QRCode.Rows;
+
+    for Row := 0 to QRCode.Rows - 1 do
+    begin
+      for Column := 0 to QRCode.Columns - 1 do
+      begin
+        if QRCode.IsBlack[Row, Column] then
+          QRCodeBitmap.Canvas.Pixels[Column, Row] := clBlack
+        else
+          QRCodeBitmap.Canvas.Pixels[Column, Row] := clWhite;
+      end;
+    end;
+
+    Result := QRCodeBitmap;
+  finally
+    QRCode.Free;
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+   close;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  QRCodeBitmap: TBitmap;
+begin
+  QRCodeBitmap := CriarQRcodeBitmap(Edit1.Text);
+  try
+    Image1.Picture.Bitmap.Assign(QRCodeBitmap);
+  finally
+    QRCodeBitmap.Free;
+  end;
+end;
+
+end.
